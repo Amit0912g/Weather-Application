3bb932f174ed969bc44471e4c5bdbaaf0bae92a1
Amit Gupta <amit.g09122000@gmail.com>
2025-03-28 14:40:34 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,13 @@ app.use(express.json())
 
 const weatherRoutes = require("./routes/weather.routes");
 app.use("/api/v1/weather", weatherRoutes);
+
+app.get("/",(req,res)=>{
+    res.send({
+        activeStatus:true,
+        error:false
+    })
+})
 app.listen(PORT,(err)=>{
     if(err) throw new err
     console.log(`Server running `)
